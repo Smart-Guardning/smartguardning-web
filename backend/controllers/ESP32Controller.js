e6841126c4ef945aac5eae9fa51a201a805ddcef
IceCreamPie-dev <icecreampie9000@gmail.com>
2024-06-04 13:17:53 +0900

    FIX - controllers 잘못된 구독 해결

--- backend/controllers/ESP32Controller.js
+++ backend/controllers/ESP32Controller.js
@@ -2,22 +2,40 @@
 const axios = require('axios');
 const mqttClient = require('../mqtt.js');
 
-exports.findArduinos = async (req, res) => {
-  try {
-    mqttClient.subscribe('arduinos', function (err) {
+let isSubscribed = false;
+let sensorDataCache = {}; // 데이터를 캐시할 객체
+
+function initializeMqtt() {
+  if (!isSubscribed) {
+    mqttClient.subscribe('smartfarm/sensor/#', function (err) {
       if (err) {
-        console.error('Error subscribing to arduinos:', err);
-        res.status(500).send(err);
+        console.error('Error subscribing to topic:', err);
       } else {
-        mqttClient.on('message', function (topic, message) {
-          if (topic === 'arduinos') {
-            const arduinos = JSON.parse(message.toString());
-            const nodeIds = arduinos.map(arduino => arduino.node_id); // node_id만 추출
-            res.json(nodeIds);
-          }
-        });
+        console.log('Subscribed to topic: smartfarm/sensor/#');
+        isSubscribed = true;
       }
     });
+
+    mqttClient.on('message', function (topic, message) {
+      if (topic.startsWith('smartfarm/sensor/')) {
+        const sensorData = JSON.parse(message.toString());
+        const nodeId = sensorData.node_id;
+        sensorDataCache[nodeId] = sensorData; // 메시지 데이터를 저장
+      }
+    });
+  }
+}
+
+initializeMqtt();
+
+exports.findArduinos = async (req, res) => {
+  try {
+    const nodeIds = Object.keys(sensorDataCache); // 저장된 노드 ID 목록
+    if (nodeIds.length > 0) {
+      res.json(nodeIds);
+    } else {
+      res.status(404).send('No data available');
+    }
   } catch (error) {
     console.error('Error in findArduinos:', error);
     res.status(500).send(error);
